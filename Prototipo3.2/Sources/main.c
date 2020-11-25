65302a0168eee3fe55905a54cc3fc0f056961cb3
riaj0224 <jair2000.0224@hotmail.com>
2020-11-25 15:36:54 -0600

    3.2

---
+++ Prototipo3.2/Sources/main.c
@@ -0,0 +1,58 @@
+/*
+ * main implementation: use this 'C' sample to create your own application
+ *
+ */
+
+
+#include "derivative.h" 
+#include "system_init.h"
+#include "system_IRQ.h"
+#include "system_functions.h"
+
+extern unsigned long rpmA;									//Speed of the wheelA by encoder
+extern unsigned long rpmB;									//Speed of the wheelB by encoder
+extern unsigned short duty_cycleA;							//The duty_cycle of motor1
+extern unsigned short duty_cycleB;							//The duty cycle of motor2
+extern unsigned char paro;
+
+
+extern char buffer[800];
+extern char to_parse[100];
+extern unsigned char found_char;
+extern unsigned char buff_ind;
+extern unsigned char messageComplete;
+
+
+
+
+
+
+void FTM0_IRQHandler();
+void PIT_IRQHandler();
+void UART1_IRQHandler();
+
+int main(void)
+{
+	int counter = 0;
+	
+	CLK_init ();
+	PORT_init();
+	TPM0_init();
+	TPM1_init();
+	TPM2_init();
+	I2C0_init();
+	IMU_setup();
+	UART0_init();
+	UART1_init();
+	WiFi_setup();
+	PIT_init();
+	
+	
+	for(;;) {	   
+	   	counter++;
+	   	WiFi_execute_instructions();
+
+	}
+	
+	return 0;
+}
---
+++ Prototipo3.2/Sources/system_IRQ.h
@@ -0,0 +1,16 @@
+/*
+ * system_IRQ.h
+ *
+ *  Created on: Oct 21, 2020
+ *      Author: jair2
+ */
+
+#ifndef SYSTEM_IRQ_H_
+#define SYSTEM_IRQ_H_
+
+void FTM0_IRQHandler(void);
+void PIT_IRQHandler(void);
+void UART1_IRQHandler(void);
+
+
+#endif /* SYSTEM_IRQ_H_ */
